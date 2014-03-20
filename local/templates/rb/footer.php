61fcc692e576465f5df6a88b265b2dd3db04d54d
kisonic <kisonic@mail.ru>
2014-03-20 18:43:46 +0400

    1

---
+++ local/templates/rb/footer.php
@@ -0,0 +1,9 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+IncludeTemplateLangFile(__FILE__);
+$curpage = $APPLICATION->GetCurPage(false);
+?>
+	</section>
+	<div id="close_popup_window"></div>
+</body>
+</html>
